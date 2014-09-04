38f75e2f03eebc56e64df1263cfacdc01c0a375c
hhyuanf <stevenf@ucla.edu>
2014-09-04 12:07:28 -0400

    NeckSnake Device with an Android App

---
+++ src/com/example/necksnake/GlobalParameters.java
@@ -0,0 +1,13 @@
+package com.example.necksnake;
+
+public class GlobalParameters {
+	public static boolean isGaming = false;
+	public static boolean isMonitor = false;
+	public static int threshold = 72;
+	public static int moniteringTime = 3600000;
+	public static int intervalTime = 1800000;
+	public static long startNew=20000;
+	public static long startTime = 20000;
+	public static long accumulatedTime = 0;
+	//public static boolean finished = false;
+}
---
+++ src/com/example/necksnake/StartActivity.java
@@ -0,0 +1,51 @@
+package com.example.necksnake;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start);
+		Button btnMonitor = (Button)findViewById(R.id.monitor);
+		Button btnSetting = (Button)findViewById(R.id.setting);
+		Button btnGame = (Button)findViewById(R.id.game);
+		if (GlobalParameters.isGaming) {
+			btnMonitor.setEnabled(false);
+			btnSetting.setEnabled(false);
+		}
+		if (GlobalParameters.isMonitor) {
+			btnGame.setEnabled(false);
+			btnSetting.setEnabled(false);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.start, menu);
+		return true;
+	}
+	
+	public void playGame(View view) {
+	    Intent intent = new Intent(this, Snake.class);
+	    startActivity(intent);
+	}
+	
+	public void setting(View view) {
+	    Intent intent = new Intent(this, Setting.class);
+	    startActivity(intent);
+	}
+	
+	public void monitor(View view) {
+		Intent intent = new Intent(this, Monitor.class);
+		startActivity(intent);
+	}
+ 
+}
